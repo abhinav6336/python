19cdad10e8ac1f0b76442dc0f3319094cc3b6af7
Abhinav prakash <abhinav115ereve@gmail.com>
2026-03-18 20:07:48 +0530

    Refactor generator usage in generators.py
    Comment out initial print statements and iterate through generator.

--- generators.py
+++ generators.py
@@ -2,6 +2,8 @@ def square_nums(nums):
 	for i in nums:
 			yield (i*i)
 mynums = square_nums([1,2,3,4,5,6,7,8])
-print (next(mynums))
-print (next(mynums))
-print (next(mynums))
+#print (next(mynums))
+#print (next(mynums))
+#print (next(mynums))
+for i in mynums:
+	print(i)
