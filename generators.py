2a7726788a3e317228a2a84a26f61187fff0e0b1
Abhinav prakash <abhinav115ereve@gmail.com>
2026-03-19 19:28:47 +0530

    Enhance generators.py with examples of yielding values
    Updated the generators.py file to demonstrate different ways of using generators, including yielding values and list comprehensions.

--- generators.py
+++ generators.py
@@ -1,9 +1,20 @@
+#USING  YEILDING 
 def square_nums(nums):
 	for i in nums:
 			yield (i*i)
 mynums = square_nums([1,2,3,4,5,6,7,8])
+#print (next(mynums)) #prints each i*i
 #print (next(mynums))
 #print (next(mynums))
-#print (next(mynums))
-for i in mynums:
-	print(i)
+print(list(mynums))
+#for i in mynums:
+#	print (i)
+
+
+#USING GENERATORS TO YIELD
+mynums = (x*x for x in [1,2,3,4,5,6,7,8])
+print(mynums)
+
+#USING GENERATORS IN LIST
+mynums = [x*x for x in [1,2,3,4,5,6,7,8]]
+print(mynums)
